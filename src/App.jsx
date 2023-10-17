017e520df05f95eec0c0cef5558e62cf392b9130
Ana Claudia <anacnogueira@gmail.com>
2023-10-17 19:50:59 -0300

    Componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Ana Claudia"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab ipsa laboriosam blanditiis earum fugit alias beatae, amet dolores vel voluptates autem eius itaque veniam placeat mollitia consectetur nulla nesciunt!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Ana Claudia"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab ipsa laboriosam blanditiis earum fugit alias beatae, amet dolores vel voluptates autem eius itaque veniam placeat mollitia consectetur nulla nesciunt!"
+          />
+          <Post
+            author="Ana Claudia"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab ipsa laboriosam blanditiis earum fugit alias beatae, amet dolores vel voluptates autem eius itaque veniam placeat mollitia consectetur nulla nesciunt!"
+          />
+        </main>
+      </div>
     </>
   );
 }
