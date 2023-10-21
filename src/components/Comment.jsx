4e19945ae5b7fc870b0f1e3cfe25ea966f19ea91
Ana Claudia <anacnogueira@gmail.com>
2023-10-21 20:06:35 -0300

    Component Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://githug.com/anacnogueira.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Ana Claudia Nogueira</strong>
+              <time
+                title="20 de outubro às 11:55"
+                dateTime="2023-10-20 11:55:00"
+              >
+                Cerca de 2h atrás
+              </time>
+            </div>
+            <button title="Deletar Comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
