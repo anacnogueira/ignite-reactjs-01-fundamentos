d430ee8827a86958df2c478965d04fcadfefa0c6
Ana Claudia <anacnogueira@gmail.com>
2023-10-22 15:12:10 -0300

    Iterando no JSX Posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,45 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "http://github.com/anacnogueira.png",
+      name: "Ana Claudia Nogueira",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-10-03 20:00:05"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://github.com/anacnogueira.png",
+      name: "Ana Claudia Nogueira",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-10-13 20:00:05"),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -12,14 +51,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Ana Claudia"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab ipsa laboriosam blanditiis earum fugit alias beatae, amet dolores vel voluptates autem eius itaque veniam placeat mollitia consectetur nulla nesciunt!"
-          />
-          <Post
-            author="Ana Claudia"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab ipsa laboriosam blanditiis earum fugit alias beatae, amet dolores vel voluptates autem eius itaque veniam placeat mollitia consectetur nulla nesciunt!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
