d0340dc37bd87e722aa57879e4cde8064c6ab399
Ana Claudia <anacnogueira@gmail.com>
2023-10-31 20:20:32 -0300

    Entendo Closures no React

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -12,8 +12,11 @@ export function Comment({ content, onDeleteComment }) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1);
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/anacnogueira.png" />
